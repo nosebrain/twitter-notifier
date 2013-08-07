9518dfc0d86099b7827bc40209ea756fde61958b
wbiller <wbiller@localhost>
2013-08-07 14:02:14 +0200

    Properties are part of the specific implementation.

--- src/main/java/de/nosebrain/twitter/notifier/Notifier.java
+++ src/main/java/de/nosebrain/twitter/notifier/Notifier.java
@@ -1,7 +1,5 @@
 package de.nosebrain.twitter.notifier;
 
-import java.util.Properties;
-
 import twitter4j.Status;
 
 /**
@@ -17,12 +15,4 @@ public interface Notifier {
    * @param status
    */
   public void notify(Status status);
-
-  /**
-   * set up the notifier
-   * 
-   * @param properties
-   */
-  public void setupNotifier(final Properties properties);
-
 }
